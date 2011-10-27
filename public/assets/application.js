b5f17841de4b84d71f30db7ff3cae19541c25924
Robert E. Proscia <bob.proscia@gmail.com>
2011-10-27 02:33:49 -0400

    precompiled assets

--- public/assets/application.js
+++ public/assets/application.js
@@ -1,6 +1,2 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
